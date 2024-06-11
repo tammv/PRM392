00fb6b726592eb8db6b40926eae407075fc6c475
Tammv <72090950+tammv@users.noreply.github.com>
2024-06-11 16:14:58 +0700

    EX14 -EX15

---
+++ EX14/app/src/main/java/com/example/ex14/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.ex14;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.content.SharedPreferences;
+import android.content.Context;
+import android.view.View;
+public class MainActivity extends AppCompatActivity {
+    EditText editTextName;
+    EditText editTextEmail;
+    private Button buttonSave;
+    private Button buttonLoad;
+    private TextView textViewResult;
+    private static final String SHARED_PREFS = "sharedPrefs";
+    private static final String NAME_KEY = "name";
+    private static final String EMAIL_KEY = "email";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        editTextName = findViewById(R.id.editTextName);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        buttonSave = findViewById(R.id.buttonSave);
+        buttonLoad = findViewById(R.id.buttonLoad);
+        textViewResult = findViewById(R.id.textViewResult);
+    }
+    // Save name and email to Shared Preferences
+    public void onSave(View view) {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(NAME_KEY, editTextName.getText().toString());
+        editor.putString(EMAIL_KEY, editTextEmail.getText().toString());
+        editor.apply();
+    }
+
+    // Get name and email from Shared Preferences
+    public void onLoad(View view) {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        String name = sharedPreferences.getString(NAME_KEY, "");
+        String email = sharedPreferences.getString(EMAIL_KEY, "");
+        textViewResult.setText("Name: " + name + "\nEmail: " + email);
+    }
+}
---
+++ EX15/app/src/main/java/com/example/ex15/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.ex15;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+public class MainActivity extends AppCompatActivity {
+    private EditText nameEditText, phoneEditText;
+    private TextView resultTextView;
+
+    private Button addButton, queryButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main
+        ), (v, insets) -> {
+            Insets systemBars =
+                    insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top,
+                    systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        nameEditText = findViewById(R.id.nameEditText);
+        phoneEditText = findViewById(R.id.phoneEditText);
+        resultTextView = findViewById(R.id.resultTextView);
+        addButton = findViewById(R.id.addButton);
+        queryButton = findViewById(R.id.queryButton);
+        addButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                addContact();
+            }
+        });
+        queryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {queryContacts();
+                                                   }
+                                               });
+    }
+    private void addContact() {
+        String name = nameEditText.getText().toString();
+        String phone = phoneEditText.getText().toString();
+        ContentValues values = new ContentValues();
+        values.put(DBHelper.COLUMN_NAME, name);
+        values.put(DBHelper.COLUMN_PHONE, phone);
+
+        Uri newUri = getContentResolver().insert(ContactsProvider.CONTENT_URI, values);
+        resultTextView.setText("Added Contact: " + newUri.toString());
+    }
+    private void queryContacts() {
+        Cursor cursor =
+                getContentResolver().query(ContactsProvider.CONTENT_URI, null,
+                        null, null, null);
+        if (cursor != null) {
+            resultTextView.setText("");
+            while (cursor.moveToNext()) {
+                @SuppressLint("Range") int id =
+                        cursor.getInt(cursor.getColumnIndex(DBHelper.COLUMN_ID));
+                @SuppressLint("Range") String name =
+                        cursor.getString(cursor.getColumnIndex(DBHelper.COLUMN_NAME));
+                @SuppressLint("Range") String phone =
+                        cursor.getString(cursor.getColumnIndex(DBHelper.COLUMN_PHONE));
+                resultTextView.append("ID: " + id + ", Name: " +
+                        name + ", Phone: " + phone + "\n");
+            }
+            cursor.close();
+        }
+    }
+}
